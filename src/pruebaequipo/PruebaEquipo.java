06096d7ffcc4b3123e976e94107c3b6c5cfd8047
Envy <Envy@DESKTOP-7I42Q68>
2021-04-30 18:40:02 -0600

    Codigo Base para experimento

---
+++ src/pruebaequipo/PruebaEquipo.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ */
+package pruebaequipo;
+
+/**
+ *
+ * @author Envy
+ */
+public class PruebaEquipo {
+
+    /**
+     * @Bryan Sigala Jaime
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola soy Bryan Sigala");
+        
+        
+    }
+    
+}
